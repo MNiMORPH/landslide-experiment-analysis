65c03794cfb9954fa84f03c381d48164530f7a3c
Andy Wickert <andy@northernwidget.com>
2020-09-06 14:32:07 -0500

    Fraction of predicted topples as f(stress~size)

--- Landslides/slide_or_topple.py
+++ Landslides/slide_or_topple.py
@@ -67,6 +67,15 @@ print( np.sum(triangular_failure_notopple) / float(len(triangular_failure_notopp
 print( np.mean(TS_topple / C_slide) )
 # Just 1.2x stress at topple
 
+# Cohesion estimate at failure for predicted topples vs. predicted slides
+print("MEDIAN:")
+print("Topple 'cohesion':", np.nanmedian(C_slide[topple_preferred]))
+print("Slide cohesion:", np.nanmedian(C_slide[topple_preferred == False]))
+print("MEAN:")
+print("Topple 'cohesion':", np.nanmean(C_slide[topple_preferred]))
+print("Slide cohesion:", np.nanmean(C_slide[topple_preferred == False]))
+
+# Which ones occur within the predicted range of cohesions?
 fig = plt.figure(figsize=(6, 5.4))
 plt.loglog(C_slide[triangular_failures == False], TS_topple[triangular_failures == False], 'ks', alpha=.1, label='Trapezoidal slide')
 plt.loglog(C_slide[triangular_failures], TS_topple[triangular_failures], 'k^', alpha=.1, label='Triangular slide')
@@ -80,4 +89,48 @@ plt.tight_layout()
 
 if _save:
     plt.savefig('MassWasting_PhaseDiagram.svg')
+
+# Where do the predicted topples lie?
+n, bins = np.histogram(C_slide, bins=np.arange(0, 3201, 100))
+mag = (bins[:-1] + bins[1:])/2.
+freq = n/float(np.sum(n))/np.diff(bins)
+
+nT, binsT = np.histogram(C_slide[topple_preferred], bins=np.arange(0, 3201, 100))
+magT = (bins[:-1] + bins[1:])/2.
+freqT = nT/float(np.sum(n))/np.diff(bins)
+
+nS, binsS = np.histogram(C_slide[topple_preferred == False], bins=np.arange(0, 3201, 100))
+magS = (bins[:-1] + bins[1:])/2.
+freqS = nS/float(np.sum(n))/np.diff(bins)
+
+fig = plt.figure() #figsize=(6, 4.8))
+plt.plot( bins[:-1], freqT / freq, 'k-', linewidth=2 )
+plt.ylabel('Fraction of failures as topples', fontsize=16)
+plt.xlabel('Equivalent landslide cohesion at failure [Pa]', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=12)
+plt.xlim(0, 2300)
+plt.ylim(-.01,1.01)
+plt.tight_layout()
+
+if _save:
+    plt.savefig('PredictedToppleFrequency.svg')
+
+"""
+plt.figure(figsize=(6.4,4.8*.85)) # new default is 6.4, 4.8
+plt.axvspan(250, 750, fc='None', hatch='//', label='Literature-based cohesion\nof unsaturated sand')
+plt.bar(bins[:-1], freq, width=np.diff(bins), ec="k", fc='.8', align="edge")
+plt.bar(bins[:-1], freqS, width=np.diff(bins), ec="k", fc='.3', align="edge")
+ax = plt.gca()
+plt.ylabel('Probability density [Pa$^{-1}$, m$^{-1}$]', fontsize=16)
+plt.xlabel('Landslide cohesion at failure [Pa]', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=12)
+plt.xlim(0, ax.get_xlim()[-1])
+plt.legend()
+plt.twinx()
+plt.ylim(np.array(ax.get_ylim()) * float(np.sum(n)) * np.mean(np.diff(bins)))
+plt.ylabel('Number of landslides', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=12)
+plt.tight_layout()
+"""
+
 
